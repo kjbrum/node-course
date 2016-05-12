9c71aea29eea8c44089bc57079c41db9e03fe2db
Kyle Brumm <kjbrumm@gmail.com>
2016-05-12 08:25:13 -0500

    Init

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
+var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000;
-
-var middleware = require('./middleware.js');
-
-app.use(middleware.logger);
-
-app.get('/about', middleware.requireAuthentication, function(req, res) {
-    res.send('About Us');
-});
+var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function() {
-    console.log('Express server started on port ' + PORT + '!');
+http.listen(PORT, function() {
+    console.log('server started');
 });
